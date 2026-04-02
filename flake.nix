ac86241eab11c0c81649c8dbc144583392637866
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-04-02 15:13:20 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.pip
+            python3Packages.jinja2
+          ];
+        };
+      }
+    );
+}
